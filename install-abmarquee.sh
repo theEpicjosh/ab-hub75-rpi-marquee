93dfd42161d2c2ce711da3329853fbc34e3962a6
Josh Young <theepicjoshua365@gmail.com>
2025-03-14 19:10:16 -0300

    Add files via upload

---
+++ install-abmarquee.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+menu(){
+
+HEIGHT=21
+WIDTH=90
+CHOICE_HEIGHT=1
+BACKTITLE="AB-HUB75-RPI-Marquee"
+TITLE="AB-HUB75-RPi-Marquee Driver Installer."
+MENU="Welcome to the AB-HUB75-RPi-Marquee Driver Installer.
+(Adafruit Bonnet / HUB75 Displays / Raspberry Pi 5/RetroPie / Marquee)
+
+This script was created by theEpicjosh and was created for pre-bought arcade cabinets.
+
+Due to copyright reasons, we did not ship you with a RetroPie image as the firmware is and should be free.
+
+We are not affiliated with RetroPie (or any of the software included) and neither are we affiliated with Adafruit or Pixelcade, which is what we use the marquee images for.
+
+This installer script assumes you have followed ALL directions in the GitHub file:
+theEpicjosh/ab-hub75-rpi-marquee/README.md"
+
+OPTIONS=(1 "Credits"
+         2 "INSTALL")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+	1) credits ; menu ;;
+	2) installer ; menu ;;
+esac
+}
+
+function credits() {
+	dialog --clear --title "CREDITS" \
+	--msgbox \
+\
+	"
+	Pixelcade: GitHub repo 'alkine/pixelcade'" \
+\
+	10 75
+clear
+}
+
+function installer() {
+
+	ITITLE="Installing..."
+	IDESC="Installing drivers..."
+	IHEIGHT=10
+	IWIDTH=50
+
+}
+	sleep 1
+
+	IDESC="Installing Pixelcade Repository..."
+#	echo 0
+	ping -c 2 google.com
+#	cd ~/.emulationstation || exit 1
+
+#	echo 25
+	ping -c 2 google.com
+#	git clone https://github.com/alkine/pixelcade || exit 1
+
+#	echo 37
+	ping -c 2 google.com
+#	mv pixelcade marquee-images || exit 1
+
+	IDESC="Modifying files..."
+#	echo 50
+	ping -c 2 google.com
+#	cd marquee-images/system || exit 1
+#	cp default-gamecube.png default-gc.png || exit 1
+
+	IDESC="Installing Emulationstation Scripts..."
+#	echo 62
+	ping -c 2 google.com
+#	cd ~/.emulationstation || exit 1
+
+#	echo 75
+	ping -c 2 google.com
+#	git clone https://github.com/theEpicjosh/ab-hub75-rpi-marquee/marquee-scripts || exit 1
+
+#	echo 87
+	ping -c 2 google.com
+#	mv marquee-scripts scripts || exit 1
+
+#	echo 100
+	ping -c 2 google.com
+#	cd ~ || exit 1
+
+    	} | while IFS= read -r line; do
+		progress=$(echo "$line" | awk '{print $1}')
+		text=$(echo "$line" | cut -d' ' -f2-)
+		echo "$progress"
+		echo "XXX"
+		echo "$text"
+		echo "XXX"
+		sleep 0.1
+	done | dialog --title "$ITITLE" --gauge "Starting installation..." $IHEIGHT $IWIDTH 0
+
+    	dialog --clear --title "Complete!" --msgbox "The AB-HUB75-RPi-Marquee drivers have been installed!" $IHEIGHT $IWIDTH
+	dialog --clear --title "Reboot?" --yesno \
+		"You need to reboot your machine before you enable the drivers. \
+		\
+		Would you like to do this now?" \
+	$IHEIGHT $IWIDTH
+	dia_st=$?
+
+if [ "$dia_st" -eq 0 ]; then read -p "Yes."
+	fi
+}
+
+menu ;
